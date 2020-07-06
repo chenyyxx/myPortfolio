9deddb9c8e1c13fcf2e92fdd70a586a5a94b2066
chenyyxx <chenyx.eric2013@gmail.com>
2020-07-06 06:58:03 -0500

    added bakend staff

---
+++ jobs/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
+    # class Meta:
+    #     verbose_name = _("")
+    #     verbose_name_plural = _("s")
+
+    # def __str__(self):
+    #     return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})
+
