5efd1b7813a43524055a18d2d5f4313bec635200
chenyyxx <chenyx.eric2013@gmail.com>
2020-07-06 22:25:41 -0500

    Added detail page for my personal portfolio

--- jobs/models.py
+++ jobs/models.py
@@ -8,8 +8,8 @@ class Job(models.Model):
     #     verbose_name = _("")
     #     verbose_name_plural = _("s")
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.summary
 
     # def get_absolute_url(self):
     #     return reverse("_detail", kwargs={"pk": self.pk})
