8ce314024ede853f35f0de2736183c42f7fd5135
chenyyxx <chenyx.eric2013@gmail.com>
2020-07-08 23:19:42 -0500

    Added basic pages and components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+  render(){
+    return(
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/data_scientist" component={DataScientistPage} />
+          <Route exact path='/software_engineer' component={SoftwareEngineerPage} />
+          <Route exact path="/mobile" component={Mobile}/>
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
